ae64266ab2b05bb0c13581cbafa895b6870fc4dd
BonTeves43 <bonteves43@gmail.com>
2025-10-15 23:05:21 +0800

    update dynamic routing page.tsx

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -3,8 +3,7 @@ interface DocumentIdPageProps {
         Promise <{documentId: string; }>
 }
 const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
-    const awaitedParams = await params;
-    const documentId = awaitedParams.documentId;
+    const {documentId} = await params;
     return <div>Document ID: {documentId} </div>;
 }
 export default DocumentIdPage;
