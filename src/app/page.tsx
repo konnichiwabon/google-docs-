6238038b00272dc9d5853d29824edeb823b6288f
BonTeves43 <bonteves43@gmail.com>
2025-10-15 22:55:04 +0800

    for dynamic routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button";
 
 export const Home = () => {
   return <div>
-    <p className="flex min-h-screen items-center justify-center  ">Home </p>
+    <p className="flex min-h-screen items-center justify-center  justify-left">
+      <Button>Home </Button>
+      </p>
   </div>
 }
 
-export default Home;
+export default Home; 
