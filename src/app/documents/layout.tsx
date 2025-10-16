d10acac8e3c518fdf4a6a0f817db2f563385a08f
BonTeves43 <bonteves43@gmail.com>
2025-10-16 11:38:16 +0800

    create document->layout

---
+++ src/app/documents/layout.tsx
@@ -0,0 +1,10 @@
+interface DocumentsLayoutProps {
+    children: React.ReactNode;
+}
+
+const DocumentsLayout = ({children}: DocumentsLayoutProps) => {
+  return <div className="flex flex-col gap-y-4">
+    <nav className="w-full bg-red-500">Document nabar </nav>
+    {children}</div>;
+}
+export default DocumentsLayout;
